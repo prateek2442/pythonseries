0163c26d14987e53063bf4774807b3aa3217b8a5
Prateek singh <devsin2442@gmail.com>
2026-06-19 15:19:47 +0530

    daily

--- if_statements.py
+++ if_statements.py
@@ -23,3 +23,23 @@ elif operator == "/":
 
 else:
     print("Operator not supported")
+
+
+#Exercise 2 - python weight converter
+
+weight = float(input("enter your weight:"))
+unit = input("kilogram or pound ? : (K or L)")
+
+if unit == "K":
+    weight = weight * 2.205
+    unit = "lbs"
+    print(f"Your weight in kilogram is, {weight} {unit}")
+
+elif unit == "L":
+    weight = weight / 2.205
+    unit = "kgs"
+    print(f"Your weight in pound is, {weight} {unit}")
+
+else:
+    print("Operator not supported")
+
